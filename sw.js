badfb2067bb76ce60abd2ae4acd69297c9961c59
funortehub <ian.santos2639@soufunorte.com.br>
2025-10-31 21:23:38 -0300

    Refactor service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,44 +1,56 @@
-const CACHE_NAME = 'terminal-med-cache-v1';
-const urlsToCache = [
+
+const CACHE_NAME = 'axonevo-pwa-v2';
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/assets/icon.svg'
+  '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        // We don't cache external resources during install
+        // as they can fail and break the SW installation.
+        // They will be cached on the first fetch.
+        return cache.addAll(URLS_TO_CACHE);
       })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  // Ignoramos requisições para Firebase e Imgur para não cacheá-las
-  if (event.request.url.includes('firebase') || event.request.url.includes('imgur')) {
+self.addEventListener('fetch', (event) => {
+  // We only want to cache GET requests.
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
+  // Don't cache Firebase requests as they need to be fresh.
+  if (event.request.url.includes('firestore.googleapis.com') || event.request.url.includes('firebaseapp.com')) {
     return;
   }
 
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    caches.open(CACHE_NAME).then((cache) => {
+      return fetch(event.request).then((networkResponse) => {
+        // If the fetch is successful, update the cache
+        cache.put(event.request, networkResponse.clone());
+        return networkResponse;
+      }).catch(() => {
+        // If the network fails, try to serve from cache
+        return cache.match(event.request);
+      });
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
@@ -46,4 +58,5 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  return self.clients.claim();
 });
